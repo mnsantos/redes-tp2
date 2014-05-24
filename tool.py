b92ff2da3c9d2a6e777a0f81bea4a25b5c546f2f
Martín Santos <martin@martin-pc.(none)>
2014-05-24 20:18:50 -0300

    tool

--- tool.py
+++ tool.py
@@ -7,6 +7,18 @@ from scapy.all import *
 import sys
 
 if __name__ == '__main__':
-	ans,unans = sr(IP(dst = sys.argv[1], ttl=4) / ICMP(type=13))
-	#ans.make_table(lambda (s,r): (s.dst, s.ttl, r.src ))
-	ans.summary()
+
+	for i in range(0,20):
+		pkt = IP(dst = sys.argv[1], ttl=1)  / ICMP()
+		ans,unans = sr(pkt, timeout=1)
+
+		if len(ans)!=0:
+			print "src: " + ans[0][0].src + " respuesta: " + ans[0][1].src
+			print str((ans[0][1].time-ans[0][0].sent_time)*1000) +" ms" #RTT
+			print ans[0][0].id
+			ans = []
+		else:
+			print "No hubo respuesta"
+		
+		#ans.summary()
+		#unans.summary()
